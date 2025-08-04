5550a04baebcf1b1a1d37b1996e7320110137c46
MurilojrMarques <muriloj2702@gmail.com>
2025-08-04 00:33:29 -0300

    feat: add CORS middleware and dashboard route for user-specific houses

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import SessionController from './controller/SessionController';
 import HouseController from './controller/HouseController';
 import uploadFiles from './config/uploadFiles';
 import multer from 'multer';
+import DashboardController from './controller/DashboardController';
 
 const routes = new Router();
 const upload = multer(uploadFiles);
@@ -13,5 +14,8 @@ routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 routes.get('/houses', HouseController.index);
 routes.put('/houses/:id', upload.single('thumbnail'), HouseController.update);
 routes.delete('/houses/:house_id', HouseController.destroy);
+
+
+routes.get('/dashboard', DashboardController.show);
 
 export default routes;
